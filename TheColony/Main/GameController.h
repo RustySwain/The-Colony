2836256616c7be5039d4f57b2f874a336ca23de4
Rusty Swain <redstonerusty@gmail.com>
2017-02-27 16:22:41 -0500

    game controller

--- TheColony/Main/GameController.h
+++ TheColony/Main/GameController.h
@@ -1,18 +1,20 @@
 #pragma once
 #include "GameObject.h"
-#include <DirectXMath.h>
+#include "DirectXMath.h"
 
-class PlayerController : public Component
+class GameController : public Component
 {
-	const unsigned int id = 21;
+	const unsigned int id = 23;
 
 public:
-	PlayerController();
-	~PlayerController();
+	GameController();
+	~GameController();
 
 	// Component
 	virtual const unsigned int GetId() const override { return id; };
 	virtual void Start() override;
 	virtual void Update() override;
 	virtual void OnDelete() override;
+
+	static DirectX::XMFLOAT3 GridSquareFromTerrain(DirectX::XMFLOAT3 _terrainLoc);
 };
--- TheColony/Main/PlayerController.cpp
+++ TheColony/Main/PlayerController.cpp
@@ -21,10 +21,3 @@ void PlayerController::Update()
 void PlayerController::OnDelete()
 {
 }
-
-XMFLOAT3 PlayerController::GridSquareFromTerrain(XMFLOAT3 _terrainLoc)
-{
-	GameObject* terrain = GameObject::FindFromTag("Terrain")[0];
-	XMFLOAT4 pos = terrain->GetComponent<MeshRenderer>()->GetMesh()->GetVertexData()[(int)_terrainLoc.z * terrain->GetComponent<Terrain>()->GetWidth() + (int)_terrainLoc.x].position;
-	return XMFLOAT3(pos.x, pos.y, pos.z);
-}
