6a97e5ee32316a00e54311657086ff99168e8bad
Rusty Swain <redstonerusty@gmail.com>
2017-02-27 18:33:43 -0500

    why was that in namespace DirectX?????

--- TheColony/Main/GameController.h
+++ TheColony/Main/GameController.h
@@ -2,27 +2,26 @@
 #include "GameObject.h"
 #include "DirectXMath.h"
 
+using namespace DirectX;
+
 enum JOB_ENUM { No_Job = 0, Farmer, Builder, Forester, Miner, Metalworker, Teacher, Physician };
-enum ITEM_ENUM { Wood,  };
+enum ITEM_ENUM { Wood, };
 
-namespace DirectX
+class GameController : public Component
 {
-	class GameController : public Component
-	{
-		const unsigned int id = 23;
+	const unsigned int id = 23;
 
-	public:
-		GameController();
-		~GameController();
+public:
+	GameController();
+	~GameController();
 
-		// Component
-		virtual const unsigned int GetId() const override { return id; };
-		virtual void Start() override;
-		virtual void Update() override;
-		virtual void OnDelete() override;
+	// Component
+	virtual const unsigned int GetId() const override { return id; };
+	virtual void Start() override;
+	virtual void Update() override;
+	virtual void OnDelete() override;
 
-		static XMFLOAT3 GridSquareFromTerrain(XMFLOAT3 _terrainLoc);
-		void AStar(XMFLOAT3);
-		void FindJob(JOB_ENUM _job);
-	};
-}
+	static XMFLOAT3 GridSquareFromTerrain(XMFLOAT3 _terrainLoc);
+	void AStar(XMFLOAT3);
+	void FindJob(JOB_ENUM _job);
+};
