acb872dd79d3bc119280f0c37c31056747cd45d8
Rusty Swain <redstonerusty@gmail.com>
2017-02-28 13:04:07 -0500

    building predictor

---
+++ TheColony/Main/BuildingPredictor.cpp
@@ -0,0 +1,21 @@
+#include "BuildingPredictor.h"
+
+BuildingPredictor::BuildingPredictor()
+{
+}
+
+BuildingPredictor::~BuildingPredictor()
+{
+}
+
+void BuildingPredictor::Start()
+{
+}
+
+void BuildingPredictor::Update()
+{
+}
+
+void BuildingPredictor::OnDelete()
+{
+}
