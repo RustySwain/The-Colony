07b574dd959bbba2f89c304d729bbaf0acb29ae6
Rusty Swain <redstonerusty@gmail.com>
2017-03-02 18:22:35 -0500

    building editor done

--- Tools/BuildingCreator/Form1.Designer.cs
+++ Tools/BuildingCreator/Form1.Designer.cs
@@ -37,6 +37,8 @@
 			this.label1 = new System.Windows.Forms.Label();
 			this.radioButton1 = new System.Windows.Forms.RadioButton();
 			this.radioButton2 = new System.Windows.Forms.RadioButton();
+			this.radioButton3 = new System.Windows.Forms.RadioButton();
+			this.radioButton4 = new System.Windows.Forms.RadioButton();
 			this.menuStrip1.SuspendLayout();
 			this.SuspendLayout();
 			// 
@@ -46,7 +48,7 @@
             this.fileToolStripMenuItem});
 			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
 			this.menuStrip1.Name = "menuStrip1";
-			this.menuStrip1.Size = new System.Drawing.Size(495, 24);
+			this.menuStrip1.Size = new System.Drawing.Size(514, 24);
 			this.menuStrip1.TabIndex = 0;
 			this.menuStrip1.Text = "menuStrip1";
 			// 
@@ -63,21 +65,21 @@
 			// loadToolStripMenuItem
 			// 
 			this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
-			this.loadToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+			this.loadToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
 			this.loadToolStripMenuItem.Text = "Load";
 			this.loadToolStripMenuItem.Click += new System.EventHandler(this.loadToolStripMenuItem_Click);
 			// 
 			// saveToolStripMenuItem
 			// 
 			this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
-			this.saveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+			this.saveToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
 			this.saveToolStripMenuItem.Text = "Save";
 			this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
 			// 
 			// exitToolStripMenuItem
 			// 
 			this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-			this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+			this.exitToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
 			this.exitToolStripMenuItem.Text = "Exit";
 			this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
 			// 
@@ -88,6 +90,7 @@
 			this.panel1.Size = new System.Drawing.Size(400, 400);
 			this.panel1.TabIndex = 1;
 			this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+			this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
 			// 
 			// label1
 			// 
@@ -110,7 +113,7 @@
 			this.radioButton1.TabStop = true;
 			this.radioButton1.Text = "Building";
 			this.radioButton1.UseVisualStyleBackColor = false;
-			this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+			this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton_CheckedChanged);
 			// 
 			// radioButton2
 			// 
@@ -123,12 +126,41 @@
 			this.radioButton2.TabStop = true;
 			this.radioButton2.Text = "Buffer";
 			this.radioButton2.UseVisualStyleBackColor = false;
+			this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton_CheckedChanged);
+			// 
+			// radioButton3
+			// 
+			this.radioButton3.AutoSize = true;
+			this.radioButton3.BackColor = System.Drawing.Color.Yellow;
+			this.radioButton3.Location = new System.Drawing.Point(425, 173);
+			this.radioButton3.Name = "radioButton3";
+			this.radioButton3.Size = new System.Drawing.Size(75, 17);
+			this.radioButton3.TabIndex = 5;
+			this.radioButton3.TabStop = true;
+			this.radioButton3.Text = "Front Door";
+			this.radioButton3.UseVisualStyleBackColor = false;
+			this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton_CheckedChanged);
+			// 
+			// radioButton4
+			// 
+			this.radioButton4.AutoSize = true;
+			this.radioButton4.BackColor = System.Drawing.Color.White;
+			this.radioButton4.Location = new System.Drawing.Point(425, 196);
+			this.radioButton4.Name = "radioButton4";
+			this.radioButton4.Size = new System.Drawing.Size(52, 17);
+			this.radioButton4.TabIndex = 6;
+			this.radioButton4.TabStop = true;
+			this.radioButton4.Text = "Erase";
+			this.radioButton4.UseVisualStyleBackColor = false;
+			this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton_CheckedChanged);
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(495, 443);
+			this.ClientSize = new System.Drawing.Size(514, 443);
+			this.Controls.Add(this.radioButton4);
+			this.Controls.Add(this.radioButton3);
 			this.Controls.Add(this.radioButton2);
 			this.Controls.Add(this.radioButton1);
 			this.Controls.Add(this.label1);
@@ -155,6 +187,8 @@
 		private System.Windows.Forms.Label label1;
 		private System.Windows.Forms.RadioButton radioButton1;
 		private System.Windows.Forms.RadioButton radioButton2;
+		private System.Windows.Forms.RadioButton radioButton3;
+		private System.Windows.Forms.RadioButton radioButton4;
 	}
 }
 
