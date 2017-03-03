16ba939b116dec39f714d659f8a39c38a9e82706
Rusty Swain <redstonerusty@gmail.com>
2017-03-03 11:06:30 -0500

    builder job mostly done

--- TheColony/Main/Builder.h
+++ TheColony/Main/Builder.h
@@ -4,6 +4,25 @@
 class Builder : public JobABC
 {
 public:
+	struct Task
+	{
+		enum TASK_TYPE { BUILD, GATHER };
+		TASK_TYPE type;
+		GameObject* job = nullptr;
+		unsigned int maxWorkers;
+		vector<Builder*> workers;
+		unsigned int requiredWork;
+		unsigned int workDone = 0;
+	};
+
+private:
+	static vector<Task*> tasks;
+	Task* currentTask = nullptr;
+	float reachDistance = 1.0f;
+
+	static void FinishTask(unsigned int _index);
+
+public:
 	Builder();
 	~Builder();
 
@@ -11,4 +30,6 @@ public:
 	void Start() override;
 	void Update() override;
 	void OnDelete() override;
+
+	static void AddTask(GameObject* _job, Task::TASK_TYPE _taskType, unsigned int _maxWorkers);
 };
